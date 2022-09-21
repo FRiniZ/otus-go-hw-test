c6a61307212817aaef3b2ec49881df7e2803e951
Renat.Fayzullin AKA Rini <renat@cinarra.com>
2022-09-21 12:27:41 +0300

    HW3 is completed#1

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,47 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+    "regexp"
+    "sort"
+    "strings"
+)
+
+var regSplitByDelimiters = regexp.MustCompile(`(?m)([\p{L}][\p{L}-]*)`)
+
+type word struct {
+    w string
+    n int
+}
+
+func Top10(str string) []string {
+    var result []string
+
+    m := make(map[string]int)
+    var sw []word
+
+    result = regSplitByDelimiters.FindAllString(str, -1)
+    for i := 0; i < len (result); i++ {
+	    m[strings.ToLower(result[i])]++
+    }
+
+    for key, value := range m {
+	sw = append (sw, word{w: key, n:value})
+    }
+
+    sort.Slice(sw, func(i, j int) bool {
+	var r bool = false
+	if sw[i].n > sw[j].n {
+	    r = true
+	} else if (sw[i].n == sw[j].n && sw[i].w < sw[j].w) {
+	    r = true
+	}
+	return r
+	})
+
+    result = nil
+    for i:= 0; i < 10 && i < len (sw); i++ {
+	result = append (result, sw[i].w)
+    }
+
+    return result
 }
