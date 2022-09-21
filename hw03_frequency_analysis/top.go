7ab27f8a093a0ae2b5f9e07cd3a7afc932aa8cc0
Renat.Fayzullin AKA Rini <renat@cinarra.com>
2022-09-21 12:30:38 +0300

    HW3 is completed#2

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,47 +1,47 @@
 package hw03frequencyanalysis
 
 import (
-    "regexp"
-    "sort"
-    "strings"
+	"regexp"
+	"sort"
+	"strings"
 )
 
 var regSplitByDelimiters = regexp.MustCompile(`(?m)([\p{L}][\p{L}-]*)`)
 
 type word struct {
-    w string
-    n int
+	w string
+	n int
 }
 
 func Top10(str string) []string {
-    var result []string
+	var result []string
 
-    m := make(map[string]int)
-    var sw []word
+	m := make(map[string]int)
 
-    result = regSplitByDelimiters.FindAllString(str, -1)
-    for i := 0; i < len (result); i++ {
-	    m[strings.ToLower(result[i])]++
-    }
-
-    for key, value := range m {
-	sw = append (sw, word{w: key, n:value})
-    }
-
-    sort.Slice(sw, func(i, j int) bool {
-	var r bool = false
-	if sw[i].n > sw[j].n {
-	    r = true
-	} else if (sw[i].n == sw[j].n && sw[i].w < sw[j].w) {
-	    r = true
+	result = regSplitByDelimiters.FindAllString(str, -1)
+	for i := 0; i < len(result); i++ {
+		m[strings.ToLower(result[i])]++
 	}
-	return r
+
+	sw := make([]word, 0)
+	for key, value := range m {
+		sw = append(sw, word{w: key, n: value})
+	}
+
+	sort.Slice(sw, func(i, j int) bool {
+		r := false
+		if sw[i].n > sw[j].n {
+			r = true
+		} else if sw[i].n == sw[j].n && sw[i].w < sw[j].w {
+			r = true
+		}
+		return r
 	})
 
-    result = nil
-    for i:= 0; i < 10 && i < len (sw); i++ {
-	result = append (result, sw[i].w)
-    }
+	result = nil
+	for i := 0; i < 10 && i < len(sw); i++ {
+		result = append(result, sw[i].w)
+	}
 
-    return result
+	return result
 }
