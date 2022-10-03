cfb8ff37c368574d913a7622d35a66d9131896cc
Renat.Fayzullin AKA Rini <renat@cinarra.com>
2022-10-03 19:24:36 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,66 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (lc *lruCache) Set (key Key, value interface{}) bool {
+    flag := false
+
+    if elm, ok := lc.items[key]; ok {
+	elm.Value = cacheItem{key, value}
+	lc.queue.MoveToFront (elm)
+	flag = true
+    } else {
+
+	if lc.queue.Len() >= lc.capacity {
+	    elm = lc.queue.Back()
+	    ci, ok := elm.Value.(cacheItem)
+	    if ok {
+		delete (lc.items, ci.key)
+		lc.queue.Remove (elm)
+	    } else {
+		panic ("Listitem does not contain cacheItem struct")
+	    }
+	}
+
+	elm = &ListItem{cacheItem{key, value}, nil, nil}
+	lc.queue.PushFront (elm)
+	lc.items[key] = elm
+    }
+    return flag
+}
+
+func (lc *lruCache) Get (key Key)(interface{}, bool) {
+    var i  interface{}
+    flag := false
+
+    if elm, ok := lc.items[key]; ok {
+	ci, ok := elm.Value.(cacheItem)
+	if ok {
+	    i = ci.value
+	} else {
+	    panic ("Listitem does not contain cacheItem struct")
+	}
+	lc.queue.MoveToFront (elm)
+	flag = true
+    }
+
+    return i, flag
+}
+
+func (lc *lruCache) Clear () {
+    for i := lc.queue.Front(); i != nil; {
+	ci, ok := i.Value.(cacheItem)
+	if ok {
+	    delete (lc.items, ci.key)
+	    lc.queue.Remove (i)
+	} else {
+	    panic ("Listitem does not contain cacheItem struct")
+	}
+    }
 }
 
 type cacheItem struct {
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,97 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+    head	*ListItem
+    tail	*ListItem
+    size	 int
+}
+
+func (l *list) Len() int {
+
+    return (l.size)
+}
+
+func (l *list) Front () *ListItem {
+    return (l.head)
+}
+
+func (l *list) Back () *ListItem {
+    return (l.tail)
+}
+
+func (l *list) PushFront (v interface{}) *ListItem {
+    elm := &ListItem{v, nil, nil}
+
+    if l.size == 0 {
+	l.tail = elm
+    } else {
+	l.head.Prev = elm
+	elm.Next = l.head
+    }
+
+    l.head = elm
+    l.size++
+
+    return (l.head)
+}
+
+func (l *list) PushBack (v interface{}) *ListItem {
+    elm := &ListItem{v, nil, nil}
+
+    if l.size == 0 {
+	l.head = elm
+    } else {
+	l.tail.Next = elm
+	elm.Prev = l.tail
+    }
+
+    l.tail = elm
+    l.size++
+
+    return (l.tail)
+}
+
+func (l *list) Remove (i *ListItem) {
+
+    if i ==  nil {
+	return
+    }
+
+    elm_prev := i.Prev
+    elm_next := i.Next
+
+    if elm_prev != nil {
+	elm_prev.Next =  elm_next
+    }
+
+    if elm_next != nil {
+	elm_next.Prev = elm_prev
+    }
+
+    l.size--
+}
+
+func (l *list) MoveToFront (i *ListItem) {
+
+    if i == l.head {
+	return
+    }
+
+    elm_prev := i.Prev
+    elm_next := i.Next
+
+    if elm_prev != nil {
+	elm_prev.Next =  elm_next
+    }
+
+    if elm_next != nil {
+	elm_next.Prev = elm_prev
+    }
+
+    i.Prev = nil
+    i.Next = l.head
+    l.head.Prev = i
+    l.head = i
 }
 
 func NewList() List {
