f9c034fa2456f65c438b9a4dc05c9ff3c62171e1
Renat.Fayzullin AKA Rini <renat@cinarra.com>
2022-10-26 08:05:07 +0300

    HW06 : Done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,30 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	worker := func(done In, chIn In) Out {
+		ch := make(chan interface{})
+		go func() {
+			defer close(ch)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-chIn:
+					if !ok {
+						return
+					}
+					ch <- v
+				}
+			}
+		}()
+		return ch
+	}
+
+	for _, s := range stages {
+		out = worker(done, s(out))
+	}
+
+	return out
 }
