79eb50202325e26c8644d87793302b1838b7ce07
Renat.Fayzullin AKA Rini <renat@cinarra.com>
2022-11-15 14:08:31 +0300

    HW7 : Done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +13,57 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	var err_last error = nil
+
+	in, err := os.Open (fromPath)
+	if err != nil {
+		return err
+	}
+
+	fi, err := in.Stat()
+	if err != nil {
+		return err
+	}
+
+	defer in.Close()
+
+	out, err := os.Create (toPath)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if offset > 0 {
+	    if (offset > fi.Size()) {
+		return ErrOffsetExceedsFileSize
+	    }
+	    in.Seek (offset, 0)
+	}
+
+	limit_r := limit
+	if limit_r == 0 || (fi.Size() > 0 &&limit_r > fi.Size() - offset) {
+	    limit_r = fi.Size() - offset
+	}
+
+	in_limit := io.LimitReader (in, limit_r)
+
+	bar := pb.Full.Start64(limit_r)
+
+	barReader := bar.NewProxyReader(in_limit)
+
+	for {
+		_, err := io.CopyN(out, barReader, 1024)
+		if err != nil {
+		    if err == io.EOF {
+			break
+		    }
+		    err_last = err	
+		    break;
+		}
+	}
+
+	bar.Finish()
+
+	return err_last
 }
