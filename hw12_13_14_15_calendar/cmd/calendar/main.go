667d535e0562045a8c82b4ed8783af44674b8098
Renat.Fayzullin AKA Rini <renat@cinarra.com>
2023-01-21 03:35:35 +0300

    HW14 : Try to pass linter

--- hw12_13_14_15_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_calendar/cmd/calendar/main.go
@@ -15,12 +15,12 @@ import (
 )
 
 func main() {
-	conf := NewConfig()
-	storage := storage.NewStorage(conf.Storage)
-	logger := logger.NewLogger(conf.Logger.Level, os.Stdout)
-	calendar := app.NewCalendar(logger, conf.CalendarConf, storage)
-	httpsrv := internalhttp.NewServer(logger, calendar, conf.HTTP.Host, conf.HTTP.Port)
-	grpcsrv, _ := internalgrpc.NewServer(logger, calendar, conf.GRPC.Host, conf.GRPC.Port)
+	config := NewConfig()
+	storage := storage.NewStorage(config.Storage)
+	logger := logger.NewLogger(config.Logger.Level, os.Stdout)
+	calendar := app.NewCalendar(logger, config.CalendarConf, storage)
+	httpsrv := internalhttp.NewServer(logger, calendar, config.HTTP.Host, config.HTTP.Port)
+	grpcsrv, _ := internalgrpc.NewServer(logger, calendar, config.GRPC.Host, config.GRPC.Port)
 
 	calendar.Run(httpsrv, grpcsrv)
 
